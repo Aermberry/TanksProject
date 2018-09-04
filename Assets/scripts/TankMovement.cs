25d54f702103545c0f1e9bbe3aed5b863c2cd77b
Salmonberry <sos2012happy@gmail.com>
2018-09-04 21:10:37 +0800

    Update:function: turn

---
+++ Assets/scripts/TankMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMovement : MonoBehaviour {
+
+	public float speed = 5;
+	public float angularSpeed = 30;
+	private Rigidbody rigidbody;
+	// Use this for initialization
+	void Start () {
+		rigidbody = this.GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		float v = Input.GetAxis ("Vertical");
+		rigidbody.velocity = transform.forward * speed * v;
+
+		float h = Input.GetAxis ("Horizontal");
+		
+		rigidbody.angularVelocity = transform.up * angularSpeed * h;
+	}
+}
